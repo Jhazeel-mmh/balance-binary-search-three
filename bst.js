466aa4b7a04b53de645e659b871822f21a6dd462
Jhazeel Hernandez <140919471+Jhazeel-mmh@users.noreply.github.com>
2025-05-02 13:39:42 -0600

    Add the prettyPrint() method to the Tree class

--- bst.js
+++ bst.js
@@ -68,4 +68,26 @@ class Tree{
         }
         return root;
     }
+
+    prettyPrint(){
+        this.__prettyPrint(this.root)
+    }
+
+    __prettyPrint(node, prefix = "", isLeft = true){
+        if (node === null) {
+          return;
+        }
+        if (node.right !== null) {
+          this.__prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+          this.__prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+      };
+     
 }
+
+const myBbst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+myBbst.prettyPrint()
